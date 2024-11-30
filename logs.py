d1d2206b6971180a92468d72376020e8f8c0947b
Mohammadnaved Naved <36394261+navedmohammad@users.noreply.github.com>
2024-11-30 12:02:40 +0530

    Update logs.py

--- logs.py
+++ logs.py
@@ -3,15 +3,19 @@ from logging.handlers import RotatingFileHandler
 
 logging.basicConfig(
     level=logging.ERROR,
-    format=
-    "%(asctime)s - %(levelname)s - %(message)s [%(filename)s:%(lineno)d]",
+    format="%(asctime)s - %(levelname)s - %(message)s [%(filename)s:%(lineno)d]",
     datefmt="%d-%b-%y %H:%M:%S",
     handlers=[
         RotatingFileHandler("logs.txt", maxBytes=50000000, backupCount=10),
         logging.StreamHandler(),
     ],
 )
+
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
+# Use a different name for the logger instance
+logger = logging.getLogger()
 
-logging = logging.getLogger()
+# Example usage
+logger.error("This is an error message.")
+logger.info("This info message will not be logged due to the level set to ERROR.")
